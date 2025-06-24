315fbc190089db4a6e1e6cebdb8e5117f01d5030
Usman-17 <usmandev580@gmail.com>
2025-06-24 16:57:58 +0500

    update base url

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = "https://chatapp-vzob.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
